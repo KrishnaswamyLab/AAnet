b606e1f9acb283106fe9781158307b9e7bac12cd
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2020-10-14 12:28:08 -0400

    Adding code to project a simplex onto a sphere

--- AAnet_torch/AAnet.py
+++ AAnet_torch/AAnet.py
@@ -3,6 +3,7 @@ from torch import nn, optim
 import numpy as np
 import sklearn
 
+from .utils import get_n_simplex
 
 class AAnet(nn.Module):
     def __init__(
@@ -166,44 +167,3 @@ class AAnet(nn.Module):
         '''
         X_bary = self.euclidean_to_barycentric(archetypal_embedding)
         return torch.mean(self.dist_to_simplex(X_bary) ** 2)
-
-    def train_epoch(model, data_loader):
-        loss = 0
-        reconstruction_loss = 0
-        archetypal_loss = 0
-        for batch_features, _ in train_loader:
-            # reshape mini-batch data to [N, 784] matrix
-            # load it to the active device
-            batch_features = batch_features.view(-1, 784).to(device)
-    
-            # reset the gradients back to zero
-            # PyTorch accumulates gradients on subsequent backward passes
-            optimizer.zero_grad()
-    
-            # compute reconstructions
-            output, archetypal_embedding = model(batch_features)
-            
-            # compute training reconstruction loss
-            curr_reconstruction_loss = torch.mean((output - batch_features)**2)
-            reconstruction_loss += curr_reconstruction_loss
-            
-            # compute training archetypal loss
-            curr_archetypal_loss = model.calc_archetypal_loss(archetypal_embedding)
-            archetypal_loss += curr_archetypal_loss
-            
-            train_loss = curr_reconstruction_loss + curr_archetypal_loss
-            
-            # compute accumulated gradients
-            train_loss.backward()
-    
-            # perform parameter update based on current gradients
-            optimizer.step()
-    
-            # add the mini-batch training loss to epoch loss
-            loss += train_loss.item()
-    
-        # compute the epoch training loss
-        loss = loss / len(train_loader)
-        reconstruction_loss = reconstruction_loss / len(train_loader)
-        archetypal_loss = archetypal_loss / len(train_loader)
-        return loss, reconstruction_loss, archetypal_loss
---
+++ AAnet_torch/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+
+import AAnet_torch.AAnet
+import AAnet_torch.utils
