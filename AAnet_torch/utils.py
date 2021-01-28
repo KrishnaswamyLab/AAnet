c567787dcaa0abe6987d7d94b851ca9313550942
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2021-01-28 09:03:05 -0500

    fixing merge

--- AAnet_torch/utils.py
+++ AAnet_torch/utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 import torch
 
-<<<<<<< HEAD
-=======
 def _barycentric_to_euclidean(X_barycentric, simplex_radius):
     '''
     Converts an M x (N + 1) array of barycentric coordinates into an M x N array
@@ -61,7 +59,7 @@ def sample_from_stereo_sphere_simplex(n_dim=3, simplex_radius=1.0, n_samples=100
     '''
     Samples non-uniformly from an N-dimensional simplex projected onto an
     N-dimensional sphere using a stereographic projection.
-    
+
     (Note that a 'stereographic spherical simplex' is _not_ the same thing as a
     spherical simplex defined using the submanifold geometry the N-dimensional sphere
     inherets from (N + 1)-dimensional Euclidean space.)
@@ -90,66 +88,6 @@ def sample_from_stereo_sphere_simplex(n_dim=3, simplex_radius=1.0, n_samples=100
     print(X_sphere.shape)
 
     return X_sphere
-
-def get_n_simplex(n=2, scale=1, device=None):
-    '''
-    Returns an n-simplex centered at the origin
-    '''
-
-    nth = 1/(n-1)*(1-np.sqrt(n)) * np.ones(n-1)
-    D = np.vstack([np.eye(n-1), nth]) * scale
-    return torch.tensor(D - np.mean(D, axis=0), device=device)
-
-
-def generate_data_on_sphere(n_obs=1000, radius=1):
-    '''
-    Generates data on that lies on a sphere of `radius`.
-
-    TODO: not convinced that this is the correct way to project points onto sphere
-          also doesn't scale to more than 3 dimensions :(
-    '''
-    np.random.seed(0)
-
-    n_obs = 2000
-    n_dim = 2
-    n_archetypes = 3
-
-    # Get coordinates of archetypes in R2
-    archetypes = get_n_simplex(n=n_archetypes).numpy()
-
-    # Uniformly sample from a simplex
-    data = np.random.uniform(0, 1, size=[n_obs, n_archetypes])
-    data = -np.log(data)
-    data = data / np.sum(data, axis=1, keepdims=True)
-
-    # Project the points to appear within the archetypal coordiantes
-    data_atype = data @ archetypes
-
-    # 2D points on simplex to points on a sphere
-    data_sphere  = _latlong_to_xyz(data_atype, radius=radius)
-    atype_sphere = _latlong_to_xyz(archetypes, radius=radius)
-
-    # Center the data
-    data_center = data_sphere.mean(axis=0, keepdims=True)
-    data_sphere -= data_center
-    atype_sphere -= data_center
-
-    return data_sphere, atype_sphere
-
-
-def _latlong_to_xyz(X, radius=1):
-    '''
-    Takes 2D input as lat/long coordinates and returns coordinates in R3
-    '''
-    longitude = X[:,0] / radius
-    latitude = 2 * np.arctan(np.exp(X[:,1]/radius)) - np.pi/2
-    x3 = radius * np.cos(latitude) * np.cos(longitude)
-    y3 = radius * np.cos(latitude) * np.sin(longitude)
-    z3 = radius * np.sin(latitude)
-    #x3 = (x3 - R) / 10
-    return np.vstack([x3, y3, z3]).T
-
->>>>>>> 4d8aa301b5b79bdcb553248bb83812db44ef449d
 
 def train_epoch(model, data_loader):
     loss = 0
